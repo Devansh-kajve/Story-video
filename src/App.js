01d5eb9d0121ae2d8b1bfc3c2f84cf5f03e39dfa
Devansh-kajve <devanshkajve123@gmail.com>
2024-06-24 18:47:28 +0530

    Added server.js and created github workflow file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 import Form from "./Components/Form";
+import ImageUpload from "./Components/ImageUpload";
+import Video from "./Components/Video";
 
 function App() {
+  const handleImageUpload = (file) => {
+    // Here you can perform actions with the uploaded file, such as displaying it or sending it to a server
+    console.log("Uploaded file:", file);
+  };
   return (
     <div className="App">
+      {/* <ImageUpload onUpload={handleImageUpload} /> */}
       <Form />
+      {/* <Video /> */}
     </div>
   );
 }
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,18 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Form.css";
 import axios from "axios";
+import Loader from "./../loader.svg";
 
 const Form = () => {
-  // State to hold the value of the text area
   const [textValue, setTextValue] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [imageSrc, setImageSrc] = useState([]);
+  const [frames, setFrames] = useState([]);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [apiKey, setApiKey] = useState("");
 
-  const apiKey = process.env.REACT_APP_API_KEY;
-  const imageUrls = [];
+  // const apiKey = process.env.REACT_APP_API_KEY;
 
-  // Function to handle changes in the text area
+  useEffect(() => {
+    const fetchConfig = async () => {
+      try {
+        const response = await fetch("/config.json");
+        const config = await response.json();
+        setApiKey(config.apiKey);
+      } catch (error) {
+        console.error("Error fetching configuration:", error);
+      }
+    };
+
+    fetchConfig();
+  }, []);
+
   const handleTextChange = (e) => {
     setTextValue(e.target.value);
   };
@@ -21,12 +36,9 @@ const Form = () => {
     setIsLoading(true);
     setError("");
     try {
-      // Split the text input into an array of lines based on full stops
       const linesArray = textValue.split(".");
-      // Remove any empty lines from the array
       const filteredArray = linesArray.filter((line) => line.trim() !== "");
 
-      // Call the API for each line in the filtered array
       const imagePromises = filteredArray.map(async (line) => {
         const formData = new FormData();
         formData.append("prompt", line);
@@ -45,22 +57,19 @@ const Form = () => {
         );
 
         if (response.status === 200) {
-          // Handle the image data, such as saving it to a file or displaying it
-          console.log("Image data:", response.data);
           const base64String = arrayBufferToBase64(response.data);
-          const imagesUrl = `data:image/png;base64,${base64String}`;
-          imageUrls.push(imagesUrl);
-          setImageSrc(imageUrls);
-        }
-
-        if (response && response.status === 400) {
-          // Handle the 400 error response
+          const imageUrl = `data:image/png;base64,${base64String}`;
+          return imageUrl;
+        } else if (response.status === 400) {
           console.log("Bad request:", response.data);
+          throw new Error("Bad request");
+        } else {
+          throw new Error("Unexpected response status");
         }
       });
 
-      // Wait for all image requests to complete
-      await Promise.all(imagePromises);
+      const generatedImages = await Promise.all(imagePromises);
+      setImageSrc((prevImages) => [...prevImages, ...generatedImages]);
     } catch (error) {
       console.error("API request error:", error);
       setError("Error fetching images. Please try again.");
@@ -69,7 +78,6 @@ const Form = () => {
     }
   };
 
-  // Function to convert ArrayBuffer to base64 string
   const arrayBufferToBase64 = (buffer) => {
     let binary = "";
     const bytes = new Uint8Array(buffer);
@@ -80,28 +88,90 @@ const Form = () => {
     return window.btoa(binary);
   };
 
+  const handleThumbnailClick = (src) => {
+    setSelectedImage(src);
+  };
+
+  const handleUseClick = () => {
+    if (selectedImage) {
+      setFrames((prevFrames) => [...prevFrames, selectedImage]);
+      console.log("Frames:", frames);
+    }
+  };
+
+  const sendFramesToServer = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.post(
+        "http://localhost:3001/trigger-render",
+        { frames }
+      );
+      console.log("Render triggered:", response.data.message);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error triggering render:", error);
+      setError("Failed to trigger video render. Please try again.");
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="form-container">
-      {/* Text area */}
-      <textarea
-        className="textarea-input"
-        placeholder="Enter text here"
-        value={textValue}
-        onChange={handleTextChange}
-      />
-
-      {/* Button to trigger the onClick function */}
-      <button className="button" onClick={handleClick}>
-        Click me
-      </button>
-      {imageSrc.map((src, index) => (
-        <img
-          key={index}
-          src={src}
-          alt={`Generated Image ${index + 1}`}
-          style={{ marginRight: "10px" }}
+      <div className="input-container">
+        <input
+          type="text"
+          className="input-field"
+          placeholder="Enter text here"
+          value={textValue}
+          onChange={handleTextChange}
         />
-      ))}
+        <button className="button" onClick={handleClick}>
+          Generate
+        </button>
+      </div>
+      {isLoading && <img src={Loader} />}
+
+      {error && <p className="error">{error}</p>}
+      <h1>Image preview:</h1>
+      <div className="image-preview-container">
+        {selectedImage && (
+          <div className="selected-image-container">
+            <img
+              src={selectedImage}
+              alt="Selected"
+              className="selected-image"
+            />
+            <button className="use-button" onClick={handleUseClick}>
+              Use
+            </button>
+          </div>
+        )}
+        <div className="thumbnail-container">
+          {imageSrc.map((src, index) => (
+            <img
+              key={index}
+              src={src}
+              alt={`Generated Image ${index + 1}`}
+              className="thumbnail"
+              onClick={() => handleThumbnailClick(src)}
+            />
+          ))}
+        </div>
+      </div>
+      <h1>Frames preview:</h1>
+      <div className="thumbnail-container">
+        {frames.map((src, index) => (
+          <img
+            key={index}
+            src={src}
+            alt={`Generated Image ${index + 1}`}
+            className="thumbnail"
+          />
+        ))}
+      </div>
+      <button onClick={sendFramesToServer} disabled={frames.length === 0}>
+        Render Video
+      </button>
     </div>
   );
 };
